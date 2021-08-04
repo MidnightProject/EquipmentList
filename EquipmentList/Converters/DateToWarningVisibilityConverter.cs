1ec918cdea3916e90eb890c64f6bcb3a06a6d343
MidnightProject <54016876+MidnightProject@users.noreply.github.com>
2021-08-04 23:20:36 +0200

    feat: Update warning column in equipment view

---
+++ EquipmentList/Converters/DateToWarningVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EquipmentList.Converters
+{
+    public class DateToWarningVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToDateTime() == new DateTime())
+            {
+                return Visibility.Collapsed;
+            }
+
+            if (value.ToDateTime() < DateTime.Now)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
