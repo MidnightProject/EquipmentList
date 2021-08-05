41eca68e4e19b7484c3bdae3bb85cc683c9d2437
MidnightProject <54016876+MidnightProject@users.noreply.github.com>
2021-08-05 13:53:30 +0200

    feat: Add resize to warning column in equipment view

--- EquipmentList/View/Equipment.xaml.cs
+++ EquipmentList/View/Equipment.xaml.cs
@@ -1,21 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
-using TomsToolbox.Essentials;
 
 namespace EquipmentList.View
 {
     public partial class Equipment : UserControl
     {
+        private Boolean dataGridIsInitialize = true;
+        private int defaultValue = -1;
+        private List<int> cellWidth = new List<int>();
+        private int maxWidth = 0;
+
         public Equipment()
         {
             InitializeComponent();
+
+            DataGrid.Columns[0].Width = 0;
 
             Dispatcher.BeginInvoke(DispatcherPriority.ContextIdle, new Action(() =>
             {
                 DataGrid.SelectedIndex = -1;
             }));
+        }
+
+        private void WarningColumn_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (dataGridIsInitialize)
+            {
+                if (defaultValue ==  -1)
+                {
+                    defaultValue = (int)e.NewSize.Width;
+                }
+                
+                if ((int)e.PreviousSize.Width == 0 && defaultValue == (int)e.NewSize.Width)
+                {
+                    cellWidth.Add((int)e.NewSize.Width);
+                }
+                else
+                {
+                    dataGridIsInitialize = false;
+                }
+            }
+
+            if (!dataGridIsInitialize)
+            {
+                if (maxWidth < (int)e.NewSize.Width)
+                {
+                    maxWidth = (int)e.NewSize.Width;
+                }
+
+                cellWidth.Remove((int)e.PreviousSize.Width);
+
+                if (maxWidth == (int)e.PreviousSize.Width);
+                {
+                    maxWidth = cellWidth.Max();
+                }
+
+                cellWidth.Add((int)e.NewSize.Width);
+            }
+
+            DataGrid.Columns[0].Width = maxWidth + 5;
         }
     }
 }
