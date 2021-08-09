25777f7e24ab813784ac0a65529fd05e7ba062e2
MidnightProject <54016876+MidnightProject@users.noreply.github.com>
2021-08-09 14:43:58 +0200

    feat: Update warning column in equipment view

---
+++ EquipmentList/Converters/AlarmToWarningVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EquipmentList.Converters
+{
+    public class AlarmToWarningVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0].ToDateTime().AddDays((int)values[1]) < DateTime.Now)
+            {
+                return true;
+            }
+
+            if (values[2].ToDateTime().AddDays((int)values[3]) < DateTime.Now)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
